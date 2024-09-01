a4b736b8315d234c298f2c34f651865329709175
PasztorGergo <pasztorg05@gmail.com>
2024-09-01 12:07:36 +0200

    first-task-second-part

--- FirstTask.java
+++ FirstTask.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FirstTask{
@@ -10,11 +11,23 @@ public class FirstTask{
         return sum == n;
     }
 
+    public static ArrayList<Integer> perfectNumbersTo(int n){
+        ArrayList<Integer> numbers =  new ArrayList<Integer>();
+        for(int i = 1; i < n; i++){
+            if(isPerfectNumber(i))
+                numbers.add(i);
+        }
+        return numbers;
+    }
+
     public static void main(String[] args){
         System.out.println("Adjon meg egy egészszámot!");
         Scanner scanner = new Scanner(System.in);
 
         System.out.printf("A szám %stökéletes szám", (isPerfectNumber(scanner.nextInt()) ? "" : "nem "));
+
+        System.out.println("Adjon meg egy egészszámot!");
+        perfectNumbersTo(scanner.nextInt()).forEach(n -> System.out.println(n));
 
         scanner.close();
     }
